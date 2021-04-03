83b72dcc40dedefb09107db6f46b0424d769454a
Fernando <fernandopach2017@gmail.com>
2021-04-03 18:11:15 -0300

    up

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const fs = require("fs");
 const Hjson = require("hjson");
 const translate = require("google-translate-free");
-const properties = require("properties");
+
+const CONTENT_TYPE = {
+  "items":"item.",
+  "blocks":"block.",
+  "mechs":"mech.",
+  "liquids":"liquid.",
+  "units":"unit."
+}
 
 var src = fs.readdirSync("mods");
 
