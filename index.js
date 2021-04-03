d0d15b75aa5f4c0e46875c5da8ba875518cb294d
Fernando <fernandopach2017@gmail.com>
2021-04-03 17:45:53 -0300

    up

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const fs = require("fs");
 const Hjson = require("hjson");
 const translate = require("google-translate-free");
 const properties = require("properties");
+
+var src = fs.readdirSync("mods");
+
+src.forEach((modFolder) => {
+  let modSrc = fs.readdirSync(`mods/${modFolder}`);
+});
