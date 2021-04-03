336654297d3b1b780218be229f5704b3aae0579a
Fernando <fernandopach2017@gmail.com>
2021-04-03 18:27:50 -0300

    up

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const fs = require("fs");
 const Hjson = require("hjson");
 const translate = require("google-translate-free");
 
+/**
+ * @param {fs.PathLike} path
+ * @returns {Object}
+ */
+function parseContentFile (path=null) {
+  if (typeof path != "string") return {};
+
+  let parse = JSON.parse;
+  if (path.endsWith(".hjson")) parse = Hjson.parse;
+
+  
+}
+
 const CONTENT_TYPE = {
   "items":"item.",
   "blocks":"block.",
