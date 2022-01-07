1f8a797d3490d2fc06af39afde07903ca2398e65
Fernando <fernandopach2017@gmail.com>
2022-01-07 19:27:58 -0300

    up

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const fs = require("fs");
 const parseContentFile = require("./parse.js");
 
 /**
- *
- * @param {Number} s - Delay in seconds
+ * @param {Number} seconds
  * @returns
  */
-function sleep(s) {
-  return new Promise((resolve) => setTimeout(resolve, s * 1000));
+function sleep(seconds) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
+
+/**
+ * @param {Function} fn
+ * @param {Number} seconds
+ */
+function delayLoop(fn, seconds) {
+  setTimeout(fn, seconds * 1000);
 }
 
 const CONTENT_TYPE = {
@@ -19,43 +26,4 @@ const CONTENT_TYPE = {
   zones: "zone.",
 };
 
-process.on("unhandledRejection", function (err) { console.log(err) })
-
-async function makeBundle () {
-  let contentSrc = fs.readdirSync(`mods/content`);
-  modName = parseContentFile("mods/mod").name;
-
-  if (!fs.existsSync(`mods/bundles`))
-    fs.mkdirSync(`mods/bundles`);
-
-
-  var contentArr = contentSrc.map(async (contentFolder, j) => {
-    let contentFiles = fs.readdirSync(
-      `mods/content/${contentFolder}`
-    );
-
-    var contentArr2 = contentFiles.map(async (contentFile, k) => {
-      let parsedContentFile = await parseContentFile(
-        `mods/content/${contentFolder}/${contentFile}`
-      );
-      let contentFileName = contentFile.split(".")[0];
-
-      //console.log("traduzindo " + contentFile);
-      // prettier-ignore
-      let str = `${CONTENT_TYPE[contentFolder]}${modName}-${contentFileName}.name = ${parsedContentFile.name? parsedContentFile.name : "Oh No"}\n${CONTENT_TYPE[contentFolder]}${modName}-${contentFileName}.description = ${parsedContentFile.description? parsedContentFile.description : "Oh No"}\n`;
-      //console.log(str);
-      await sleep((j + k)/2);
-      return str;
-    });
-
-    return contentArr2;
-  });
-
-  let bundleFile = `mods/bundles/bundle_pt_BR.properties`;
-
-  await sleep(20);
-  console.log(contentArr.map(async c => { return await c }));
-  fs.writeFileSync(bundleFile, contentArr, { encoding: "utf8" });
-}
-
-makeBundle();
+process.on("unhandledRejection", console.log);
--- parse.js
+++ parse.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 
 var nameTest = /(?<!"?weapon"?:\s*{\s*)"?name"?:\s*"?([^\n]+)"?\b/i
-var descTest = /(?<!"?weapon"?:\s*{\s*)"?description"?:\s*"?([\w\S 	.]+)"?\b/i
+var descTest = /"?description"?:\s*"?([\w\S 	.]+)"?\b/i
 
 /**
  *
  * @param {fs.PathLike} path
  * @returns {Object}
  */
-module.exports = async function (path = null) {
+module.exports = function (path = null) {
   if (typeof path != "string") return false;
 
   if(!path.endsWith(".json") && !path.endsWith(".hjson")) {
